ccedc7a5185643947f0643734d301358f46e2488
Andy Glick <andyglick@gmail.com>
2020-04-23 09:08:38 -0400

    committing the simple changes to the word grid class

--- CCSPiJ/src/main/java/chapter3/WordGrid.java
+++ CCSPiJ/src/main/java/chapter3/WordGrid.java
@@ -1,18 +1,20 @@
-// WordGrid.java
-// From Classic Computer Science Problems in Java Chapter 3
-// Copyright 2020 David Kopec
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+/*
+ WordGrid.java
+ From Classic Computer Science Problems in Java Chapter 3
+ Copyright 2020 David Kopec
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+*/
 
 package chapter3;
 
@@ -23,7 +25,8 @@ import java.util.Random;
 public class WordGrid {
 
 	public static class GridLocation {
-		public final int row, column;
+		public final int row;
+		public final int column;
 
 		public GridLocation(int row, int column) {
 			this.row = row;
@@ -56,17 +59,15 @@ public class WordGrid {
 			if (column != other.column) {
 				return false;
 			}
-			if (row != other.row) {
-				return false;
-			}
-			return true;
-		}
+            return row == other.row;
+        }
 	}
 
-	private final char ALPHABET_LENGTH = 26;
-	private final char FIRST_LETTER = 'A';
-	private final int rows, columns;
-	private char[][] grid;
+	private static final char ALPHABET_LENGTH = 26;
+	private static final  char FIRST_LETTER = 'A';
+	private final int rows;
+	private final int columns;
+	private final char[][] grid;
 
 	public WordGrid(int rows, int columns) {
 		this.rows = rows;
@@ -162,5 +163,4 @@ public class WordGrid {
 		}
 		domain.add(locations);
 	}
-
 }
