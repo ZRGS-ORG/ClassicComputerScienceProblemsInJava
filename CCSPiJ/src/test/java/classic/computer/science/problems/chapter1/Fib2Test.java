57de20b2475dd81425ff9edbb4bd5b6ecc11ce9f
Andy Glick <andyglick@gmail.com>
2020-04-25 00:29:16 -0400

    decided to respond to sonar lint's complaints about the package names -- followed the suggestions in the regex that it posted

---
+++ CCSPiJ/src/test/java/classic/computer/science/problems/chapter1/Fib2Test.java
@@ -0,0 +1,28 @@
+package classic.computer.science.problems.chapter1;
+
+import org.junit.Test;
+
+import org.assertj.core.api.Assertions;
+
+/**
+ * @author glick
+ */
+public class Fib2Test {
+
+    @Test
+    public void testFib2() {
+
+        Fib2 fib2 = new Fib2();
+
+        System.out.println(fib2.fib2(5));
+        System.out.println(fib2.fib2(10));
+
+        Assertions.assertThat(fib2.fib2(5)).isEqualTo(5);
+        Assertions.assertThat(fib2.fib2(10)).isEqualTo(55);
+    }
+
+    @Test
+    public void exerciseFib2Main() {
+        Fib2.main(new String[]{});
+    }
+}
---
+++ CCSPiJ/src/test/java/classic/computer/science/problems/chapter1/HanoiTest.java
@@ -0,0 +1,50 @@
+package classic.computer.science.problems.chapter1;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import org.assertj.core.api.Assertions;
+
+import java.util.Stack;
+
+/**
+ * @author glick
+ */
+public class HanoiTest {
+
+    static Stack<Integer> stack = new Stack<>();
+
+    @BeforeClass
+    public static void setUp() {
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+    }
+
+    @Test
+    public void testHanoi() {
+        Hanoi hanoi = new Hanoi(3);
+
+        printCurrentDisksForTheTowers(hanoi, "hanoi towerA before execution ", "hanoi towerB before execution ", "hanoi towerC before execution ");
+
+        Assertions.assertThat(hanoi.towerA).isEqualTo(HanoiTest.stack);
+
+        hanoi.solve();
+
+        System.out.println("");
+        printCurrentDisksForTheTowers(hanoi, "hanoi towerA after execution ", "hanoi towerB after execution ", "hanoi towerC after execution ");
+
+        Assertions.assertThat(hanoi.towerC).isEqualTo(HanoiTest.stack);
+    }
+
+    private void printCurrentDisksForTheTowers(Hanoi hanoi, String s, String s2, String s3) {
+        System.out.println(s + hanoi.towerA);
+        System.out.println(s2 + hanoi.towerB);
+        System.out.println(s3 + hanoi.towerC);
+    }
+
+    @Test
+    public void exerciseHanoiMain() {
+        Hanoi.main(new String[] {});
+    }
+}
